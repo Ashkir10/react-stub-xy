cb68add2d05625cf45f17ee6a33b203187e18886
Ashkir10 <32412593+Ashkir10@users.noreply.github.com>
2018-09-13 18:07:58 +0300

    first commit...

---
+++ start-server.js
@@ -0,0 +1,7 @@
+import app from './app';
+
+app.set('port', (process.env.API_PORT || 3001));
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
